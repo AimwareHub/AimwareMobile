83c9e6e9fdeb1dcbc202b7fce9007fc087bf967d
AimwareHub <141087523+AimwareHub@users.noreply.github.com>
2023-08-13 13:47:12 -0600

    Update .lua

--- .lua
+++ .lua
@@ -341,7 +341,7 @@ plr.Character.ChildRemoved:Connect(removed)
 plr.Character.Humanoid.Jumping:Connect(jumped)
        end)
 
-            if Value == false then 
+            if (Value) == false then 
                 OrionLib:MakeNotification({
 	Name = "Aimware Mobile",
 	Content = "Aimbot V2 Turned Off",
@@ -350,7 +350,7 @@ plr.Character.Humanoid.Jumping:Connect(jumped)
 })
             end 
 
-            if Value == true then 
+            if (Value) == true then 
                 OrionLib:MakeNotification({
 	Name = "Aimware Mobile",
 	Content = "Aimbot V2 Turned On",
@@ -724,12 +724,6 @@ MainTab:AddButton({
 	Name = "Silent Aim",
 	Callback = function()
       		loadstring(game:HttpGet('https://raw.githubusercontent.com/MainHackScripts/SilentAim/main/.lua'))()
-            OrionLib:MakeNotification({
-	Name = "Aimware Mobile",
-	Content = "Doesn't Support Mobile Users, use Aimbot",
-	Image = "rbxassetid://4483345998",
-	Time = 2
-})
   	end    
 })
 
@@ -875,7 +869,7 @@ MainTab:AddSlider({
 	Default = 0,
 	Color = Color3.fromRGB(255,255,255),
 	Increment = 1,
-	ValueName = "bananas",
+	ValueName = "Mag Range",
 	Callback = function(Value)
 		_G.magRange = Value
 	end    
@@ -1033,7 +1027,7 @@ local b = w:CreateFolder("Intros")
 
 
 b:Dropdown("Intro Animations",{"Intro_AnthonyShuffle_L","Intro_Backstand_U","Intro_BillieJean_C","Intro_Boo!_L","Intro_BringItOn_U","Intro_CaramellaDance_C","Intro_ChronoSteps_R","Intro_Clap_C","Intro_ComeOn_R","Intro_CleanGroove_C","Intro_Crabby_E","Intro_Cradles_L","Intro_CrissCross_R","Intro_CutePose_C","Intro_Dab2_U","Intro_Dab_C","Intro_Dirty_R","Intro_Default","Intro_DefaultDance_U","Intro_Distraction_E","Intro_ElectroShuffle_L","Intro_ElectroSwing_U","Intro_Dribble_R","Intro_FlapperDance_U","Intro_FrameSkin_R","Intro_Freestyle_R","Intro_GetDown_U","Intro_GoBanana_E","Intro_HandShuffle_R","Intro_Hate_C","Intro_Handstand_C","Intro_Headflips_E","Intro_Hiphop_L","Intro_Headless_C","Intro_JojoGang_E","Intro_JumpingJacks_R","Intro_Kickflip_E","Intro_LDance_U","Intro_Lavish_R","Intro_LeapingDance_C","Intro_LegShake_U","Intro_Levitate_L","Intro_MarioOdyssey_R","Intro_MickyJacky_E","Intro_Mood_L","Intro_NaeNae_L","Intro_OrangeJustice_L","Intro_NanaDance_C","Intro_Roasted_U","Intro_ScoutKick_C","Intro_Poop_E","Intro_PraiseTheLord_R","Intro_Reanimated_L","Intro_Shake&Clap_E","Intro_Separate_U","Intro_ShakeItUp_U","Intro_ShowOff_U","Intro_ShuffleV2_E","Intro_Shuffle_E","Intro_SideHustle_R","Intro_Sit&Sway_C","Intro_SlapDisrespect_U","Intro_Sit_C","Intro_Slick_E","Intro_Smeeze_E","Intro_Springy_R","Intro_SmugDance_U","Intro_Swipe_R","Intro_Thriller_R","Intro_TrashCompacter_U","Intro_Wave_C","Intro_WhipIt_E","Intro_Wiggle_C","Intro_Zany_R"},true,function(mob) --true/false, replaces the current title "Dropdown" with the option that t
-game:GetService("Players").LocalPlayer.Equipping.Intro.Value = mob
+game:GetService("Players").LocalPlayer.Equipping.Intro.bool = mob
 end)
 
 
@@ -1060,7 +1054,7 @@ end)
 })
 
 local ServersTab = Window:MakeTab({
-	Name = "Player",
+	Name = "Server Features",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
@@ -1173,7 +1167,7 @@ PlayerTab:AddSlider({
 	Default = 16,
 	Color = Color3.fromRGB(255,255,255),
 	Increment = 0.1,
-	ValueName = "bananas",
+	ValueName = "Walk Speed",
 	Callback = function(Value)
 		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
 	end    
@@ -1186,7 +1180,7 @@ PlayerTab:AddSlider({
 	Default = 50,
 	Color = Color3.fromRGB(255,255,255),
 	Increment = 1,
-	ValueName = "bananas",
+	ValueName = "Jump Power",
 	Callback = function(Value)
 		game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
 	end    
